4295d551d0dfe92ec7879170b51fe10f9e31c2ac
Paul <pasha_titov@list.ru>
2018-04-15 00:36:49 +0300

    fix bugs

--- TableViewExample/DetailViewController.m
+++ TableViewExample/DetailViewController.m
@@ -22,29 +22,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-//- (void)configureView {
-//    // Update the user interface for the detail item.
-//    if (self.detailItem) {
-//        self.detailDescriptionLabel.text = [self.detailItem description];
-//    }
-//}
-//
-//- (void)setDetailItem:(NSDate *)newDetailItem {
-//    if (_detailItem != newDetailItem) {
-//        _detailItem = newDetailItem;
-//
-//        // Update the view.
-//        [self configureView];
-//    }
-//}
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
